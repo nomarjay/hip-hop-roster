eea103ec6786247e0220cccf6c3d11211850d9b5
Jordan King <105494965+nomarjay@users.noreply.github.com>
2026-02-01 00:23:02 -0500

    Use env-based API URL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-		proxy: {
-			'/api': {
-				target: 'http://localhost:8086',
-				changeOrigin: true,
-				secure: false,
-			},
-		},
-	},
+    plugins: [react()],
+    server: {
+        port: 3000,
+        proxy: {
+            '/api': {
+                target: 'http://104.236.121.210:8086',
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+    },
 });
