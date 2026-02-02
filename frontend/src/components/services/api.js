a63a148cccce855ba665ea4609d7aea3b82c3892
Jordan King <105494965+nomarjay@users.noreply.github.com>
2026-02-01 19:30:55 -0500

    update env config for server connection

--- frontend/src/components/services/api.js
+++ frontend/src/components/services/api.js
@@ -1,13 +1,10 @@
 import axios from 'axios';
 
-// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-const API_BASE_URL = '/api';
-
 const api = axios.create({
-	baseURL: API_BASE_URL,
-	headers: {
-		'Content-Type': 'application/json',
-	},
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
 });
 
 export const getAllArtists = async () => {
